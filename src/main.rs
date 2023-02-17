2216d2b010a08217f407061d6cc8b476c059652e
Max Edwards <youwontforgetthis@gmail.com>
2023-02-17 23:19:35 +0000

    first rust command

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use teloxide::prelude::*;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting Count Us In Bot");
 
     let bot = Bot::from_env();
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+    Command::repl(bot, answer).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "starts a watch party")]
+    Watch { show_name: String },
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Watch { show_name } => {
+            bot.send_message(msg.chat.id, format!("The show you want to watch is: {show_name}.")).await?
+        }
+    };
+
+    Ok(())
 }
